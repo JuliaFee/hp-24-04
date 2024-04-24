6f1497ba3261abd1e415c44d6cb1deafea511d01
JuliaFee <julia.silva133@aluno.senai.br>
2024-04-24 11:07:29 -0300

    file changes

--- db/script.sql
+++ db/script.sql
@@ -7,6 +7,7 @@ CREATE TABLE bruxos (
     nome VARCHAR(100) NOT NULL,
     idade VARCHAR(100) NOT NULL,
     genero VARCHAR(100) NOT NULL,
+    habilidade VARCHAR(100) NOT NULL,
     casa VARCHAR(100) NOT NULL,
     status_sangue VARCHAR(100) NOT NULL,
     patrono VARCHAR(100) NOT NULL
@@ -15,8 +16,15 @@ CREATE TABLE bruxos (
 CREATE TABLE varinhas (
     id SERIAL PRIMARY KEY,
     material VARCHAR(100) NOT NULL,
-    comprimento VARCHAR(100) NOT NULL,
+    comprimento INT NOT NULL,
     nucleos VARCHAR(100) NOT NULL,
     data_fab VARCHAR(100) NOT NULL
+);
+
+CREATE TABLE casas (
+    id SERIAL PRIMARY KEY,
+    nome VARCHAR(100) NOT NULL,
+    cor VARCHAR(50) NOT NULL,
+    animal VARCHAR(100) NOT NULL
 );
 
--- index.js
+++ index.js
@@ -13,3 +13,211 @@ const pool = new Pool({
 });
 
 app.use(express.json());
+
+// ----------------------------------------------------------------------------
+// rotas GET bruxos
+
+app.get('/bruxos', async (req, res) => {
+    try {
+        const resultado = await pool.query('SELECT * FROM bruxos');
+        res.json({
+            total: resultado.rowCount,
+            usuarios: resultado.rows
+        });
+    } catch (error) {
+        console.error('erro ao obter bruxos');
+        res.status(500).send({ mensagem: 'erro interno ao obter bruxos' });
+    }
+});
+
+app.get('/bruxos/:id', async (req, res) => {
+    try {
+        const { id } = req.params;
+        const resultado = await pool.query('SELECT * FROM bruxos WHERE id = $1', [id]);
+        res.json({
+            total: resultado.rowCount,
+            bruxos: resultado.rows
+        });
+    } catch (error) {
+        console.error('erro ao obter bruxo');
+        res.status(500).send({ mensagem: 'erro interno ao obter bruxo' });
+    }
+});
+
+// rota POST bruxos
+
+app.post('/bruxos', async (req, res) => {
+    try {
+        const { nome, idade, genero, habilidade, casa, status_sangue, patrono } = req.body;
+        await pool.query('INSERT INTO bruxos (nome, idade, genero, habilidade, casa, status_sangue, patrono) VALUES ($1, $2, $3, $4, $5, $6, $7)', 
+        [nome, idade, genero, habilidade, casa, status_sangue, patrono]);
+    } catch (error) {
+        console.error('erro ao registrar bruxo');
+        res.status(500).send({ mensagem: 'erro interno ao registrar bruxo' });
+    }
+});
+
+// rota PUT bruxos
+
+app.put('/bruxos/:id', async (req, res) => {
+    try {
+        const { id } = req.params;
+        const { nome, idade, genero, habilidade, casa, status_sangue, patrono } = req.body;
+        await pool.query('UPDATE bruxos SET nome = $1, idade = $2, genero = $3, habilidade = $4, casa = $5, status_sangue = $6, patrono = $7 WHERE id = $8', 
+        [nome, idade, genero, habilidade, casa, status_sangue, patrono, id]);
+    } catch (error) {
+        console.error('erro ao atualizar bruxo');
+        res.status(500).send({ mensagem: 'erro interno ao atualizar bruxo' });
+    }
+});
+
+// rota DELETE bruxos
+
+app.delete('/bruxos/:id', async (req, res) => {
+    try {
+        const { id } = req.params;
+        await pool.query('DELETE FROM bruxos WHERE id = $1', [id]);
+        res.status(200).send({ mensagem: `bruxo de id ${id} excluido com sucesso` });
+    } catch (error) {
+        console.error('erro ao excluir bruxo');
+        res.status(500).send({ mensagem: 'erro interno ao excluir bruxo' });
+    }
+});
+// ----------------------------------------------------------------------------
+
+// rota GET varinhas
+
+app.get('/varinhas', async (req, res) => {
+    try {
+        const resultado = await pool.query('SELECT * FROM varinhas');
+        res.json({
+            total: resultado.rowCount,
+            varinhas: resultado.rows
+        });
+    } catch (error) {
+        console.error('erro ao obter varinhas');
+        res.status(500).send({ mensagem: 'erro interno ao obter varinhas' });
+    }
+});
+
+app.get('/varinhas/:id', async (req, res) => {
+    try {
+        const { id } = req.params;
+        const resultado = await pool.query('SELECT * FROM varinhas WHERE id = $1', [id]);
+        res.json({
+            total: resultado.rowCount,
+            varinhas: resultado.rows
+        });
+    } catch (error) {
+        console.error('erro ao obter varinha');
+        res.status(500).send({ mensagem: 'erro interno ao obter varinha' });
+    }
+});
+
+// rota POST varinhas
+
+app.post('/varinhas', async (req, res) => {
+    try {
+        const { material, comprimento, nucleos, data_fab } = req.body;
+        await pool.query('INSERT INTO varinhas (material, comprimento, nucleos, data_fab) VALUES ($1, $2, $3, $4)',[material, comprimento, nucleos, data_fab]);
+    } catch (error) {
+        console.error('erro ao registrar varinha');
+        res.status(500).send({ mensagem: 'erro interno ao registrar varinha' });
+    }
+});
+
+// rota PUT varinhas
+
+app.put('/varinhas/:id', async (req, res) => {
+    try {
+        const { id } = req.params;
+        const { material, comprimento, nucleos, data_fab } = req.body;
+        await pool.query('UPDATE varinhas SET material = $1, comprimento = $2, nucleos = $3, data_fab = $4 WHERE id = $5', 
+        [material, comprimento, nucleos, data_fab, id]);
+    } catch (error) {
+        console.error('erro ao atualizar varinha');
+        res.status(500).send({ mensagem: 'erro interno ao atualizar varinha' });
+    }
+});
+
+// rota DELETE varinhas
+
+app.delete('/varinhas/:id', async (req, res) => {
+    try {
+        const { id } = req.params;
+        await pool.query('DELETE FROM varinhas WHERE id = $1', [id]);
+        res.status(200).send({ mensagem: `varinha de id ${id} excluida com sucesso` });
+    } catch (error) {
+        console.error('erro ao excluir varinha');
+        res.status(500).send({ mensagem: 'erro interno ao excluir varinha' });
+    }
+});
+
+// ----------------------------------------------------------------------------
+// rota GET casas
+
+app.get('/casas', async (req, res) => {
+    try {
+        const resultado = await pool.query('SELECT * FROM casas');
+        res.json({
+            total: resultado.rowCount,
+            casas: resultado.rows
+        });
+    } catch (error) {
+        console.error('erro ao obter as casas de hogwarts');
+        res.status(500).send({ mensagem: 'erro interno ao obter as casas de hogwarts' });
+    }
+});
+
+app.get('/casas/:id', async (req, res) => {
+    try {
+        const { id } = req.params;
+        const resultado = await pool.query('SELECT * FROM casas WHERE id = $1', [id]);
+        res.json({
+            total: resultado.rowCount,
+            casas: resultado.rows
+        });
+    } catch (error) {
+        console.error('erro ao obter a casa de hogwarts');
+        res.status(500).send({ mensagem: 'erro interno ao obter a casa de hogwarts' });
+    }
+});
+
+// rota POST casas
+
+app.post('/casas', async (req, res) => {
+    try {
+        const { nome, cor, animal } = req.body;
+        await pool.query('INSERT INTO casas (nome, cor, animal) VALUES ($1, $2, $3)', [nome, cor, animal]);
+    } catch (error) {
+        console.error('erro ao registrar a casa de hogwarts');
+        res.status(500).send({ mensagem: 'erro interno ao registrar a casa de hogwarts' });
+    }
+});
+
+// rota PUT casas
+
+app.put('/casas/:id', async (req, res) => {
+    try {
+        const { id } = req.params;
+        const { nome, cor, animal } = req.body;
+        await pool.query('UPDATE casas SET nome = $1, cor = $2, animal = $3 WHERE id = $4', [nome, cor, animal, id]);
+    } catch (error) {
+        console.error('erro ao atualizar a casa de hogwarts');
+        res.status(500).send({ mensagem: 'erro interno ao atualizar a casa de hogwarts' });
+    }
+});
+
+// rota DELETE casas 
+
+app.delete('/casas/:id', async (req, res) => {
+    try {
+        const { id } = req.params;
+        await pool.query('DELETE FROM casas WHERE id = $1', [id]);
+        res.status(200).send({ mensagem: `casa de id ${id} excluida com sucesso` });
+    } catch (error) {
+        console.error('erro ao excluir a casa de hogwarts');
+        res.status(500).send({ mensagem: 'erro interno ao excluir a casa de hogwarts' });
+    }
+});
+// ----------------------------------------------------------------------------
